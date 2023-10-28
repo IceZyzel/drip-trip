e7b1006beeda3c75eb5aa06319862a9faeda10b0
ValuteMaster413 <ilyaromanovsky303@gmail.com>
2023-10-28 17:12:00 +0300

    Update models.py

--- driptrip/drip/models.py
+++ driptrip/drip/models.py
@@ -2,24 +2,18 @@ from django.db import models
 from django import forms
 from django.core.validators import MaxValueValidator
 
-# Create your models here.
 ROLE_CHOICES = (
     ('Purchaser'),
     ('Salesperson'),
     ('Admin'),
     ('Courier'),
     )
+
 GENDER_CHOICES = (
     ('M', 'Male'),
     ('F', 'Female'),
     ('U', 'Unisex'),
     )
-class Size(models.Model):
-    text= models.CharField(max_length=64)
-
-class SizeProduct(models.Model):
-    size = models.ForeignKey(Size,on_delete=models.CASCADE)
-    product = models.ForeignKey('Product',on_delete=models.CASCADE)
 
 class User(models.Model):
     name = models.CharField(max_length=64)
@@ -32,27 +26,21 @@ class Product(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=64)
     price = models.CharField(max_length=64)
-    count = models.CharField(max_length=64)
     brand = models.CharField(max_length=64)
     description = models.TextField(max_length=250)
     sex = models.CharField(max_length=1, choices=GENDER_CHOICES)
     category = models.TextField(max_length=250)
 
+class Size(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    size = models.CharField(max_length=10)
+    count = models.PositiveBigIntegerField()
+
 class Review (models.Model):
-    order = models.ForeignKey('Order',on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     description = models.TextField(max_length=250)
     rate = models.IntegerField(validators=[MaxValueValidator(5)])
-
-class Order (models.Model):
-    address = models.CharField(max_length=64)
-    product = models.ForeignKey(Product,on_delete=models.CASCADE)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    status = models.CharField(max_length=64)
-
-class UserProduct(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE)   
-    product = models.ForeignKey(Product,on_delete=models.CASCADE)
 
 class Cart(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)  
@@ -60,4 +48,18 @@ class Cart(models.Model):
 class CartProduct(models.Model):
     cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
     product = models.ForeignKey(Product,on_delete=models.CASCADE)
- 
+    
+class Order (models.Model):
+    user_buyer = models.ForeignKey(User,on_delete=models.CASCADE)
+    user_courier = models.ForeignKey(User,on_delete=models.CASCADE)
+    cartproduct = models.ForeignKey(CartProduct,on_delete=models.CASCADE)
+    address = models.CharField(max_length=64)
+    status = models.CharField(max_length=64)
+
+class UserProduct(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)   
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+
+class PhotoProduct(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    photolink = models.CharField(max_length=100)
