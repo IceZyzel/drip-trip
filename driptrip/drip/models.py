5fa0c843f86b3646c2f413208103db3517b174f8
ValuteMaster413 <ilyaromanovsky303@gmail.com>
2023-10-30 19:02:44 +0200

    Update models.py

--- driptrip/drip/models.py
+++ driptrip/drip/models.py
@@ -55,11 +55,11 @@ class OrderDescription (models.Model):
     user_courier = models.ForeignKey(User,on_delete=models.CASCADE)
     cartproduct = models.ForeignKey(CartProduct,on_delete=models.CASCADE)
     address = models.CharField(max_length=64)
-    status = models.CharField(max_length=64)
 
 class Order (models.Model):
     user_buyer = models.ForeignKey(User,on_delete=models.CASCADE)
     orderdescription = models.ForeignKey(OrderDescription, on_delete=models.CASCADE)
+    status = models.CharField(max_length=64)
 
 class UserProduct(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)   
