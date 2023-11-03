874af4775d8aee04cc389bd957521b8bbad66efb
ValuteMaster413 <ilyaromanovsky303@gmail.com>
2023-11-04 00:12:52 +0200

    Update models.py

--- driptrip/drip/views.py
+++ driptrip/drip/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+	return HttpResponse('<h1>Home</h1>')
+ 
+def login(request):
+	return render(request, 'drip/login.html')
+
+def register(request):
+	return render(request, 'drip/register.html')
+
