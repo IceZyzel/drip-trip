781c09cb350280af1d82a9676cdfa051bb44bc21
Valerii <51902775+ValeraBurdulenko@users.noreply.github.com>
2023-11-08 16:48:17 +0100

    heder change

--- driptrip/drip/views.py
+++ driptrip/drip/views.py
@@ -14,34 +14,39 @@ def home(request):
     }
     return render(request, 'drip/home.html', context)
 
- 
-def userlogin(request):
-	if request.method == "GET":
-		return render(request, 'drip/login.html')
-	else:
-		form = LoginUserForm(request.POST) 
 
-		if form.is_valid():
-			user = authenticate(
+def userlogin(request):
+    if request.method == "GET":
+        return render(request, 'drip/login.html')
+    else:
+        form = LoginUserForm(request.POST)
+
+        if form.is_valid():
+            user = authenticate(
                 username=form.cleaned_data['username'],
                 password=form.cleaned_data['password'],
             )
-			if user is not None:
-				login(request, user, backend=None)
-				return redirect('home')
-			else:
-				return render(request, 'drip/login.html')
-		else:
-			return render(request, 'drip/login.html')
+            if user is not None:
+                login(request, user, backend=None)
+                return redirect('home')
+            else:
+                return render(request, 'drip/login.html')
+        else:
+            return render(request, 'drip/login.html')
+
 
 def register(request):
-	if request.method == "GET":
-		return render(request, 'drip/register.html')
-	else:
-		form = RegisterUserForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect('Userlogin')
-		else:
-			print(form.errors)
-			return render(request, 'drip/register.html')
+    if request.method == "GET":
+        return render(request, 'drip/register.html')
+    else:
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('Userlogin')
+        else:
+            print(form.errors)
+            return render(request, 'drip/register.html')
+
+
+def cart(request):
+    return render(request, 'drip/cart.html')
